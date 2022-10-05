bec2a3e0c20e7ed2a000620f992476f89c5de997
정휘찬(파트너사 (주)라드씨엔에스) <jung.hwichan@eland.co.kr>
2022-10-05 15:28:22 +0900

    Initial commit

---
+++ src/main/java/helloo/helloospring/controller/HellooController.java
@@ -0,0 +1,28 @@
+package helloo.helloospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HellooController {
+
+    @GetMapping("helloo")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "helloo";
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name" ) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-templates";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello"+name;
+    }
+}
